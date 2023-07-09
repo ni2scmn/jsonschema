aba40a882570945e8d12f8a28e9c78609bb272d8
Nils Schnakenberg <nils.schnakenberg@gmx.net>
2023-07-09 22:25:05 +0200

    testcases

---
+++ tests/testthat/test-integer-schema.R
@@ -0,0 +1,63 @@
+test_that("can create integer schema", {
+  testthat::expect_no_error({
+    js_schema(
+      js_integer()
+    )
+  })
+})
+
+test_that("can create integer schema with all attributes", {
+  testthat::expect_no_error({
+    js_schema(
+      js_integer(
+        .multiple_of = 2,
+        .minimum = 1,
+        .maximum = 10,
+        .exclusive_minimum = 2,
+        .exclusive_maximum = 9,
+        .required = TRUE
+      )
+    )
+  })
+})
+
+test_that("can create integer schema with all attributes and validate sucessfully", {
+  schema <- js_schema(
+    js_integer(
+      .multiple_of = 2,
+      .minimum = 1,
+      .maximum = 10,
+      .exclusive_minimum = 2,
+      .exclusive_maximum = 9,
+      .required = TRUE
+    )
+  )
+
+  request_valid <- 8 %>% 
+    jsonlite::toJSON(auto_unbox = TRUE)
+
+  testthat::expect_no_error({
+    validate(schema, request_valid)
+  })
+
+  request_no_multiple <- 9 %>% 
+    jsonlite::toJSON(auto_unbox = TRUE)
+
+  testthat::expect_error({
+    validate(schema, request_no_multiple)
+  })
+
+  request_too_low <- 0 %>% 
+    jsonlite::toJSON(auto_unbox = TRUE)
+
+  testthat::expect_error({ 
+    validate(schema, request_too_low)
+  })
+
+  request_too_high <- 11 %>% 
+    jsonlite::toJSON(auto_unbox = TRUE)
+
+  testthat::expect_error({
+    validate(schema, request_too_high)
+  })
+})
---
+++ tests/testthat/test-string-schema.R
@@ -0,0 +1,59 @@
+test_that("can create string schema", {
+  expect_no_error({
+    js_schema(
+      js_string()
+    )
+  })
+})
+
+test_that("can create string schema with all attributes", {
+  expect_no_error({
+    js_schema(
+      js_string(
+        .min_length = 1,
+        .max_length = 10,
+        .pattern = "^[a-z]+$",
+        .required = TRUE
+      )
+    )
+  })
+})
+
+test_that("can create string schema with all attributes and validate sucessfully", {
+  schema <- js_schema(
+    js_string(
+      .min_length = 1,
+      .max_length = 10,
+      .pattern = "^[a-z]+$",
+      .required = TRUE
+    )
+  )
+
+  request_valid <- "test" %>% 
+    jsonlite::toJSON(auto_unbox = TRUE)
+
+  expect_no_error({
+    validate(schema, request_valid)
+  })
+
+  request_too_short <- "" %>% 
+    jsonlite::toJSON(auto_unbox = TRUE)
+
+  expect_error({
+    validate(schema, request_too_short)
+  })
+
+  request_too_long <- "this is too long" %>% 
+    jsonlite::toJSON(auto_unbox = TRUE)
+
+  expect_error({
+    validate(schema, request_too_long)
+  })
+
+  request_invalid_pattern <- "123" %>% 
+    jsonlite::toJSON(auto_unbox = TRUE)
+
+  expect_error({
+    validate(schema, request_invalid_pattern)
+  })
+})
