eccd5b78355c72f3a87e80756881cda30657414d
Nils Schnakenberg <nils.schnakenberg@gmx.net>
2023-08-11 18:40:19 +0200

    feat: rework common keyword
    Common keywords are now a function to modify an existing schema component. Therefore we can now use it for every other component instantiation

--- R/comp_common.R
+++ R/comp_common.R
@@ -3,44 +3,73 @@
 # TODO '$schema' version?
 
 ps_common_attributes <- function(
-  entity,
-  .enum = NULL, # TODO check type of default value?
-  .const = NULL,
+  .element,
+
+  # describing attributes
   .title = NULL,
   .description = NULL,
-  .default = NULL, # TODO check type of default value?
-  .examples = NULL,
   .comment = NULL, # '$comment in schema'
-  .required
+  .examples = NULL,
+
+  # validating attributes
+  .enum = NULL, # TODO check type of default value?
+  .const = NULL,
+  .default = NULL # TODO check type of default value?
 ) {
-  
+
   stopifnot(
-    "`.enum` must be scalar vector or NULL" =
-      rlang::is_scalar_vector(.enum) || is.null(.enum),
-    "`.const` must be scalar vector or NULL" =
-      rlang::is_scalar_vector(.const) || is.null(.const),
+    "`.element` must be instance of js_schema_component" = 
+      inherits(.element, "js_schema_component"),
     "`.title` must be scalar string or NULL" =
       rlang::is_scalar_character(.title) || is.null(.title),
     "`.description` must be scalar string or NULL" =
       rlang::is_scalar_character(.description) || is.null(.description),
+    "`.comment` must be scalar string or NULL" =
+      rlang::is_scalar_character(.comment) || is.null(.comment),
+
+    # TODO which requirements must satify .examples
+    # "`.examples` must be scalar vector or NULL" =
+    #   rlang::is_scalar_vector(.examples) || is.null(.examples),
+
+    # TODO which type does enum have in case of object
+    "`.enum` must be scalar vector or NULL" =
+      inherits(.element, "js_schema_object") || rlang::is_scalar_vector(.enum) || is.null(.enum),
+
+    "`.const` must be scalar vector or NULL" =
+      rlang::is_scalar_vector(.const) || is.null(.const),
+
+
     "`.default` must be scalar string or NULL" =
       rlang::is_scalar_character(.default) || is.null(.default),
-    "`.examples` must be scalar vector or NULL" =
-      rlang::is_scalar_vector(.examples) || is.null(.examples),
-    "`.comment` must be scalar string or NULL" =
-      rlang::is_scalar_character(.comment) || is.null(.comment)
   )
 
-  structure(
+  if(inherits(.element, "js_schema_object") && !is.null(.enum)) {
+    rlang::warn("No validation for enums of object type implemented")
+  }
+
+  utils::modifyList(
+    .element,
     list(
-      enum = .enum,
-      const = .const,
       title = .title,
       description = .description,
-      default = .default,
+      comment = .comment,
       examples = .examples,
-      comment = .comment
-    ),
-    class = c("plumber_req_common", "plumber_req_schema")
+      enum = .enum,
+      const = .const,
+      default = .default
+    )
   )
+
+  # structure(
+  #   list(
+  #     enum = .enum,
+  #     const = .const,
+  #     title = .title,
+  #     description = .description,
+  #     default = .default,
+  #     examples = .examples,
+  #     comment = .comment
+  #   ),
+  #   class = c("plumber_req_common", "plumber_req_schema")
+  # )
 }
