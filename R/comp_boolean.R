d24296fea19ce635b6c4e46291af46a88e1ae28f
Nils Schnakenberg <nils.schnakenberg@gmx.net>
2023-08-13 17:46:00 +0200

    feat: include common keywords for boolean component

--- R/comp_boolean.R
+++ R/comp_boolean.R
@@ -1,8 +1,17 @@
-js_boolean <- function(
 
-) {
+#' TODO boolean
+#' 
+#' @inheritDotParams js_common_attributes
+#' 
+#' @export 
+js_boolean <- function(...) {
+
+  # common_args <- rlang::list2(...)
+  # TODO check that no other common schema keywords are used
+
   structure(
     list(),
     class = c("js_schema_boolean", "js_schema_component")
-  )
+  ) %>% 
+  js_common_attributes(...)
 }
