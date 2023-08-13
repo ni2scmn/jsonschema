48cdff9e80bd1d3e7241cd9e46b018421f051d43
Nils Schnakenberg <nils.schnakenberg@gmx.net>
2023-08-13 17:50:19 +0200

    docs: clean com_boolean.R

--- R/comp_boolean.R
+++ R/comp_boolean.R
@@ -1,14 +1,9 @@
-
 #' TODO boolean
 #' 
 #' @inheritDotParams js_common_attributes
 #' 
 #' @export 
 js_boolean <- function(...) {
-
-  # common_args <- rlang::list2(...)
-  # TODO check that no other common schema keywords are used
-
   structure(
     list(),
     class = c("js_schema_boolean", "js_schema_component")
