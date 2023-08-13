455bd812146af40fa27a725e7f11f54f12eb6123
Nils Schnakenberg <nils.schnakenberg@gmx.net>
2023-08-13 17:57:28 +0200

    feat: implement common keywords for array component

--- R/comp_array.R
+++ R/comp_array.R
@@ -1,20 +1,30 @@
 # TODO prefixItems?
 # TODO items can be set to false indicating only prefixItems can be included
 # TODO unevaluatedItems?
+
+#' TODO array
+#' 
+#' @param .items schema component describing array items
+#' @param .min_items minimum item count
+#' @param .max_items maximum item count
+#' @param .unique_items boolean indicating if items must be unique
+#' @inheritDotParams js_common_attributes
+#' 
+#' @export 
 js_array <- function(
-  items,
+  .items,
   .min_items = NULL,
   .max_items = NULL,
   .unique_items = NULL,
-
+  ...
   # TODO additional_items
   # TODO contains, minContains, maxContains
-  .contains = NULL,
-  .min_contains = NULL,
-  .max_contains = NULL,
+  #.contains = NULL,
+  #.min_contains = NULL,
+  #.max_contains = NULL
+  ) {
 
-  # GENERIC ARGS
-  .required = FALSE) {
+    # TODO check items type? -> js_schema_component
 
     stopifnot(
       "`.min_items` must be non negative integer or NULL" =
@@ -22,20 +32,17 @@ js_array <- function(
       "`.max_items` must be non negative integer or NULL" =
         rlang::is_scalar_integerish(.max_items) || is.null(.max_items),
       "`.unique_items` must be scalar boolean or NULL" =
-        rlang::is_scalar_logical(.unique_items) || is.null(.unique_items),
-
-      "`.required` must be scalar logical" =
-        rlang::is_scalar_logical(.required)
+        rlang::is_scalar_logical(.unique_items) || is.null(.unique_items)
     )
 
     structure(
       list(
-        items = items,
+        items = .items,
         min_items = .min_items,
         max_items = .max_items,
-        unique_items = .unique_items,
-        required = .required
+        unique_items = .unique_items
       ),
       class = c("js_schema_array", "js_schema_component")
-    )
+    ) %>% 
+    js_common_attributes(...)
   }
