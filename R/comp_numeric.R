baa4f6c1e81c3b34b512cb4686a1a666a4b2a00e
Nils Schnakenberg <nils.schnakenberg@gmx.net>
2023-08-13 18:57:46 +0200

    feat: implement common keywords for numeric

--- R/comp_numeric.R
+++ R/comp_numeric.R
@@ -1,13 +1,20 @@
+#' TODO numeric
+#' 
+#' @param .multiple_of force component to be a multiple of that number
+#' @param .minimum inclusive minimum valid value
+#' @param .maximum inclusive maximum valid value
+#' @param .exclusive_minimum exclusive minimum valid value
+#' @param .exclusive_maximum exclusive maximum valid value
+#' @inheritDotParams js_common_attributes
+#' 
+#' @export 
 js_numeric <- function(
     .multiple_of = NULL,
     .minimum = NULL,
     .maximum = NULL,
     .exclusive_minimum = NULL,
     .exclusive_maximum = NULL,
-
-    # GENERIC ARGS
-    .default = NULL,
-    .required = FALSE) {
+    ...) {
 
   stopifnot(
     "`.multiple_of` must be scalar numeric or NULL" =
@@ -19,11 +26,7 @@ js_numeric <- function(
     "`.exclusive_minimum` must be scalar numeric or NULL" =
       rlang::is_scalar_double(.exclusive_minimum) || rlang::is_scalar_integer(.exclusive_minimum) || is.null(.exclusive_minimum),
     "`.exclusive_maximum` must be scalar numeric or NULL" =
-      rlang::is_scalar_double(.exclusive_maximum) || rlang::is_scalar_integer(.exclusive_maximum) || is.null(.exclusive_maximum),
-    "`.default` must be scalar string or NULL" =
-      rlang::is_scalar_character(.default) || is.null(.default),
-    "`.required` must be scalar logical" =
-      rlang::is_scalar_logical(.required)
+      rlang::is_scalar_double(.exclusive_maximum) || rlang::is_scalar_integer(.exclusive_maximum) || is.null(.exclusive_maximum)
   )
 
   structure(
@@ -32,11 +35,9 @@ js_numeric <- function(
       minimum = .minimum,
       maximum = .maximum,
       exclusive_minimum = .exclusive_minimum,
-      exclusive_maximum = .exclusive_maximum,
-
-      default = .default,
-      required = .required
+      exclusive_maximum = .exclusive_maximum
     ),
     class = c("js_schema_numeric", "js_schema_component")
-  )
+  ) %>% 
+  js_common_attributes(...)
 }
