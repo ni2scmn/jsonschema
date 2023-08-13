acee28214e1cb63a8863a73e2c999adfbb27335d
Nils Schnakenberg <nils.schnakenberg@gmx.net>
2023-08-13 17:50:40 +0200

    feat: implement common keywords for null component

--- R/comp_null.R
+++ R/comp_null.R
@@ -1,8 +1,12 @@
-js_null <- function(
-
-) {
+#' TODO null
+#' 
+#' @inheritDotParams js_common_attributes
+#' 
+#' @export 
+js_null <- function(...) {
   structure(
     list(),
     class = c("js_schema_null", "js_schema_component")
-  )
+  ) %>% 
+  js_common_attributes(...)
 }
