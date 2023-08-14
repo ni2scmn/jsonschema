1c7658f5b76dfb86f6a260e96b1d623ba2330ef7
Nils Schnakenberg <nils.schnakenberg@gmx.net>
2023-08-14 23:40:25 +0200

    refactor: split common test to existing component tests

--- tests/testthat/test-integer-schema.R
+++ tests/testthat/test-integer-schema.R
@@ -59,3 +59,39 @@ test_that("can create integer schema with all attributes and parse sucessfully",
     parse(schema, request_too_high)
   })
 })
+
+testthat::test_that("can create and validate integer schema with const attribute", {
+  demo_title <- "My title"
+  demo_description <- "My description"
+  demo_comment <- "My comment"
+  demo_const <- 3
+
+  schema <- js_schema(
+    js_integer(
+      .title = demo_title,
+      .description = demo_description,
+      .comment = demo_comment,
+      .const = demo_const
+    )
+  )
+
+  testthat::expect_equal(schema$root$title, demo_title)
+  testthat::expect_equal(schema$root$description, demo_description)
+  testthat::expect_equal(schema$root$comment, demo_comment)
+  testthat::expect_equal(schema$root$const, demo_const)
+
+  request_valid <- 3 %>%
+    jsonlite::toJSON(auto_unbox = TRUE)
+
+  testthat::expect_no_error({
+    parse(schema, request_valid)
+  })
+
+  request_invalid <- 1 %>%
+    jsonlite::toJSON(auto_unbox = TRUE)
+
+  # TODO test when implemented
+  # testthat::expect_error({
+  #   parse(schema, request_invalid)
+  # })
+})
--- tests/testthat/test-string-schema.R
+++ tests/testthat/test-string-schema.R
@@ -55,3 +55,33 @@ test_that("can create string schema with all attributes and parse sucessfully", 
     parse(schema, request_invalid_pattern)
   })
 })
+
+testthat::test_that("can create string schema with enumeration", {
+  demo_title <- "My title"
+  demo_description <- "My description"
+  demo_comment <- "My comment"
+  demo_default <- "My enum 1"
+  demo_enum <- c("My enum 1", "My enum 2")
+  demo_examples <- c("My example 1", "My example 2")
+  demo_required <- TRUE
+
+  schema <- js_schema(
+    js_string(
+      .title = demo_title,
+      .description = demo_description,
+      .comment = demo_comment,
+      .default = demo_default,
+      .enum = demo_enum,
+      .examples = demo_examples,
+      .required = demo_required
+    )
+  )
+
+  testthat::expect_equal(schema$root$title, demo_title)
+  testthat::expect_equal(schema$root$description, demo_description)
+  testthat::expect_equal(schema$root$comment, demo_comment)
+  testthat::expect_equal(schema$root$default, demo_default)
+  testthat::expect_equal(schema$root$enum, demo_enum)
+  testthat::expect_equal(schema$root$examples, demo_examples)
+  testthat::expect_equal(schema$root$required, demo_required)
+})
